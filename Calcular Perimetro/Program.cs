f73a730a3851edce70bc80b0e939e447368b8bfd
RafaelB1307 <Rafael.bastides@uabc.edu.mx>
2022-05-21 00:38:13 -0700

    Agregar archivos de proyecto.

---
+++ Calcular Perimetro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_Perimetro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vala=20;
+            int valb = 14;
+            int valc = 6;
+            int perimetro = vala + valb + valc;
+            Console.WriteLine("El perimetro del triangulo es" + perimetro);
+            Console.WriteLine("presiona cualquier tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
